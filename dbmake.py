a8d67b397f08b37870004dee4f9bf1d0184dd869
Anirudha Ramesh <anirudha.ramesh@students.iiit.ac.in>
2018-04-06 23:23:57 +0530

    day 2 something-ish pt2

--- dbmake.py
+++ dbmake.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship , sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -15,12 +15,48 @@ class Users(Base) :
 	'''Table containing user authentication details '''
 	__tablename__ = 'userauth'
 
-	username = Column(String(50), primary_key = True, nullable = False)
+	id = Column(Integer(), primary_key = True ) 
+	username = Column(String(50), unique = True, nullable = False)
 	password = Column(String(100), nullable = False)
 
 	def __init__ (self, username, password) : 
 		self.username = username 
 		self.password = password
 
+class UserData(Base) : 
+	''' Contains bio of users, interests, history(likes, comments ) , links to images  ''' 
+	__tablename__ = 'userdata'
+
+	id = Column(Integer() , primary_key = True)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	bio = Column(String(250))
+	image = Column(String(150))
+
+	user = relationship(Users)
+
+
+
+	def __init__(self) :
+		self.bio = None 
+		self.posts = None
+		self.display_picture = None
+
+	def bio_update() :
+		pass
+
+	def display_picture() :
+		pass
+
+	# more individual update things 
+
+	def all-details(self, bio, display_picture) :
+		self.bio = bio 
+		self.display_picture = display_picture
+
+
 
 Base.metadata.create_all(engine)
+
+# Person = session.query(Users).first()
+
+# print (Person.username, " " , Person.password)
--- loginbutmoreworkier.py
+++ loginbutmoreworkier.py
@@ -1,8 +1,15 @@
 from flask import Flask, flash, redirect, render_template, request, url_for, session
+from flask_sqlalchemy import SQLAlchemy
 from os import urandom
+import dbmake
+
 
 app = Flask(__name__)
 app.secret_key = urandom(26)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///elephant.db'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
@@ -17,26 +24,61 @@ def index():
 # def index():
 #    return render_template('Index.html')
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
    session.pop('username', None)
+   session.pop('_flashes', None)
    return redirect(url_for('index'))
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login/', methods = ['GET', 'POST'])
 def login():
    error = None
    
    if request.method == 'POST':
-      if request.form['username'] != 'admin' or \
-         request.form['password'] != 'admin':
-         error = 'Invalid username or password. Please try again!'
+      usercorrect =  db.session.query(dbmake.Users).filter_by(username = request.form['username'] ).first()     
+      if usercorrect == None : 
+        flash ("Sorry! Username invalid !")
+        return redirect(url_for('index'))
+      
+      if usercorrect.password != request.form['password'] : 
+          flash ("Sorry! Password incorrect!")
+          return redirect(url_for('index'))
+      
       else:
 
-        flash('You were successfully logged in')
         session['username'] = request.form['username']
         return redirect(url_for('index'))
 
    return render_template('Login.html', error = error)
+
+
+@app.route('/register/', methods = ['GET' , 'POST'])
+def register(): 
+  '''Registration form'''
+
+  if request.method == 'POST': 
+    new_user = dbmake.Users(username = request.form['username'] , password = request.form['password'])
+    usercorrect = db.session.query(dbmake.Users).filter_by(username = new_user.username).first()
+    
+    if usercorrect == None : 
+      db.session.add(new_user)
+      db.session.commit()
+      return redirect(url_for('add_details'))
+    
+    
+    flash("Sorry! Username already exists !")
+    return redirect(url_for('index'))
+
+  return render_template('register.html')
+
+@app.route('/adddetails/' , methods = ['GET' , 'POST'])
+def add_details() : 
+  ''' Fill up user details after registering '''
+
+  if request.method == 'POST' :
+    user_details = dbmake.UserData()
+    user_details.all_details()
+
 
 if __name__ == "__main__" :
    app.run(debug = True)
